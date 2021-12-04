63e4df64281ac21bd7400f0f1638637395a7f1f1
Jasnet <94023335+gorge76@users.noreply.github.com>
2021-12-03 23:22:22 -0400

    Add files via upload
    proyecto emergentes II

---
+++ Proyecto Emergentes II-PublicaTodo.com/datos.sql
@@ -0,0 +1,9 @@
+INSERT into usuarios (id_usuario, usuario, correo, clave) VALUES (1,"admin","admin@admin","admin");
+
+INSERT into usuarios (id_usuario, usuario, correo, clave) VALUES (2,"aisor","aisor@aisor","1234");
+
+INSERT into anuncios (id, titulo, contenido) VALUES (null,"SE VENDE POR OCACION","Se vende un iPhone Xs seminuevo mas info llamar al: 89239492");
+
+INSERT into anuncios (id, titulo, contenido) VALUES (null,"SE NECESITA","Un auditor en sistemas experto de Seguridad de la informacion experiencia mas de 1 año");
+
+INSERT into anuncios (id, titulo, contenido) VALUES (null,"SE COMPRA","Se compra todo tipo de articulos antiguos");
